e88fbc078c6b44bf6fc6f729ad28f895e0977bd3
Edwin Cruz <edwincruz25@unitec.edu>
2019-05-22 23:19:12 -0600

    added print

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    
+    print("holaaa")
     if request.method == "GET":
         return "Hello"
     else:
         print(request)
-        return 200
+        return jsonify("qweqw")
