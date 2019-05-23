9366e507bd9ef55e3ded671c152194b002682536
Edwin Cruz <edwincruz25@unitec.edu>
2019-05-22 20:00:12 -0600

    lets gooo

--- application.py
+++ application.py
@@ -1,9 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    if request.method == 'GET':
-        return "Hello World!"
-    if request.method == 'POST':
-        print(request)
+    return "Hello"
