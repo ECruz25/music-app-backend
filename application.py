783f7221924030d7bdbcd896f6a348fd913e63c1
Edwin <edwincruz25@UNITEC.edu>
2019-05-23 12:58:03 -0600

    Add spotify username and request token

--- application.py
+++ application.py
@@ -18,19 +18,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #     SQLALCHEMY_DATABASE_URI = os.environ['postgresql://postgres@127.0.0.1:5432/music-app-ec']
 
 
-# scope = 'user-library-read'
+scope = 'user-library-read'
 # spotify = spotipy.Spotify()
-# util.prompt_for_user_token(username, scope, client_id='5c3b13a967bc4bf598898a7eaac6e54a',
-#                            client_secret='de48c9b902314b31a712e838ffe43fa1', redirect_uri='spotify-app-ec://callback')
-spotify = spotipy.Spotify(
-    auth="BQA4u1sAYbQjc0ZXQvgSA4ikyXMPEOpBSSpOsRHWm82zSzxujqAAiG0UShTFz2DClPsbLvE7YOXk9f6HUjGusdausB6-LiR1oeJLWOC9NiJXbKlP8OCJys3bO4nLzf_-hQC1p6D8BwKhXZu6ZYx0Zg_wYcktiiS2W6EGj04qnuosSjLY8uQVvLOegyxJWoE6QIRwiyc")
+username = "lbfi9hhe1i06wly52k8996i9s"
+token = util.prompt_for_user_token(username, scope, client_id='5c3b13a967bc4bf598898a7eaac6e54a',
+                            client_secret='de48c9b902314b31a712e838ffe43fa1', redirect_uri='spotify-app-ec://callback')
 
+spotify = spotipy.Spotify(auth=token)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == "GET":
+        return jsonify(get_get_user_tracks())
         # return jsonify(get_playlist_by_user('lbfi9hhe1i06wly52k8996i9s')[21]['name'])
-        return jsonify(get_tracks_by_playlist('lbfi9hhe1i06wly52k8996i9s', get_playlist_by_user('lbfi9hhe1i06wly52k8996i9s')[21]['id']))
+        # return jsonify(get_tracks_by_playlist('lbfi9hhe1i06wly52k8996i9s', get_playlist_by_user('lbfi9hhe1i06wly52k8996i9s')[21]['id']))
 
 
 def get_playlist_by_user(user_id):
@@ -44,8 +45,10 @@ def get_tracks_by_playlist(user_id, playlist_id):
         print(track)
         SpotifyUserSongInPlaylist(
             user_id, track['added_at'], track['track']['id'], track['track']['popularity'], track['track']['explicit'])
-
     return tracks
+
+ def get_get_user_tracks():
+    return spotify.current_user_saved_tracks()
 
 
 class SpotifyUserSongInPlaylist:
