eebea01253baebc9c8dae4111ef8e915dd177d1b
Edwin Cruz <edwincruz25@unitec.edu>
2019-06-02 20:52:29 -0600

    working

--- application.py
+++ application.py
@@ -1,78 +1,100 @@
-from flask import Flask, request, jsonify
+
+from flask import Flask, request, jsonify, json
 import os
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import JSON
 import spotipy
 import spotipy.util as util
-# spotifyusersonginplaylist
+import pycountry
+import math
+import numpy
+import random
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+app.config.from_object("config.DevelopmentConfig")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'],
-    dbpass=os.environ['DBPASS'],
-    dbhost=os.environ['DBHOST'],
-    dbname=os.environ['DBNAME']
-)
+from models import SpotifyUserSongInPlaylist, User, Playlist
 
-app.config.update(
-    SQLALCHEMY_DATABASE_URI=database_uri,
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-# scope = 'user-library-read'
-# username = "lbfi9hhe1i06wly52k8996i9s"
-# client_id = '5c3b13a967bc4bf598898a7eaac6e54a'
-#redirect_uri = 'spotify-app-ec://callback'
-#client_secret='de48c9b902314b31a712e838ffe43fa1'
-
-#token = util.prompt_for_user_token(username = username, scope = scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
-
+scope = 'user-library-read'
+username = "lbfi9hhe1i06wly52k8996i9s"
+client_id = '5c3b13a967bc4bf598898a7eaac6e54a'
+redirect_uri = 'spotify-app-ec://callback'
+client_secret='de48c9b902314b31a712e838ffe43fa1'
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == "GET":
-        # return jsonify(get_get_user_tracks())
+        user_id = 'lbfi9hhe1i06wly52k8996i9s'
+        authToken = "BQBsCKenAloVzogTa-psIh0yq69U4kgoPbHNoj3mFBMsoi4yizjWJBAR06LE4tsRdJks5WvieZMLNaPCnGx8t-YW6HSirwkXmEi2nlfsY8SMtfTR6rhZ0c0hkNgoK0NRp7Ffjqy1WMMplucofW1lJU94typ4L-QZKHRN1FwPxix88OyMQCBk-PamMgHF0fNciGMncWnkLsgg_pTwd9Xk"
+        mind_aspect = "I"
+        energy_aspect = "N"
+        nature_aspect = "T"
+        tactics_aspect = "J"
+        identity_aspect = "T"
+        get_all_playlsts(authToken)
+        # query = db.session.query(SpotifyUserSongInPlaylist).filter_by(user_id=user_id)
+        # # save_tracks_by_playlist(user_id, get_playlist_by_user(user_id)[21]['id'])
+        # users = []
+        # for song in query:
+        #     users.append(song.user_id)
+
+        # if len(users) <= 0:
+        #     user = User(user_id, mind_aspect, energy_aspect, nature_aspect, tactics_aspect, identity_aspect)
+        #     db.session.add(user)
+        #     db.session.commit()
+        #     for playlistId in get_playlists_by_user(user_id, authToken):
+        #         save_tracks_by_playlist(user_id, playlistId, authToken)
         
-        return "hello from me"
-        # return jsonify(get_tracks_by_playlist('lbfi9hhe1i06wly52k8996i9s', get_playlist_by_user('lbfi9hhe1i06wly52k8996i9s')[21]['id']))
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-
-def get_playlist_by_user(user_id):
-    spotify = spotipy.Spotify(auth=request.form['data']['accessToken'])
+def get_playlists_by_user(user_id, authToken):
+    spotify = spotipy.Spotify(auth=authToken)
     playlists = spotify.user_playlists(user_id)
-    return playlists['items']
+    playlists_id = []
+    offset_amount = math.ceil(playlists['total'] / playlists['limit'])
+    for offset in numpy.arange(offset_amount):
+        playlists1 = spotify.user_playlists(user_id, offset=offset*playlists['limit'])
+        for playlist in playlists1['items']:
+            playlists_id.append(playlist['id'])
+    return playlists_id
+
+def save_tracks_by_playlist(user_id, playlist_id, authToken):
+    spotify = spotipy.Spotify(auth=authToken)
+    tracks = spotify.user_playlist_tracks(user_id, playlist_id, offset=100)
+    offset_amount = math.ceil(tracks['total'] / tracks['limit'])
+    for offset in numpy.arange(offset_amount):
+        tracks2 = spotify.user_playlist_tracks(user_id, playlist_id, offset=offset*tracks['limit'])
+        for track in tracks2['items']:
+            track_ = SpotifyUserSongInPlaylist(
+                user_id, track['added_at'], track['track']['id'], track['track']['popularity'], track['track']['explicit'])
+            db.session.add(track_)
+            db.session.commit()
 
 
-def get_tracks_by_playlist(user_id, playlist_id):
-    tracks = spotify.user_playlist_tracks(user_id, playlist_id)
-    for track in tracks['items']:
-        print(track)
-        SpotifyUserSongInPlaylist(
-            user_id, track['added_at'], track['track']['id'], track['track']['popularity'], track['track']['explicit'])
-    return tracks
+def get_all_playlsts(authToken):
+    spotify = spotipy.Spotify(auth=authToken)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    countries = ['ec', 'fr','ar','fi','no','it','lt','ph','tw','nz','ee','tr','us','sv','cr','de','cl','jp','br','hn','gt','ch','hu','ca','pe','be','my','dk','bo','pl','at','pt','se','mx','pa','uy','is','es','cz','ie','nl','sk','co','sg','id','do','lu','gb','global','py','au','lv','gr','hk']
+    mind_aspects = ['E', 'I']
+    energy_aspects = ['S', 'N']
+    nature_aspects = ['T', 'F']
+    tactics_aspects = ['J', 'P']
+    identity_aspects = ['A', 'T']
+    for i in numpy.arange(len(countries)):
+        try:
+            print("Started with: " + countries[i])
+            for letter in numpy.arange(len(letters)):
+                pl = spotify.search(letters[letter],type="playlist", market=countries[i], limit=50)
+                offset_amount = math.ceil(pl['playlists']['total'] / pl['playlists']['limit'])
+                for offset in numpy.arange(offset_amount):
+                    playlists = spotify.search(q=letters[letter],type="playlist", market=countries[i], offset=offset, limit=50)['playlists']['items']
+                    for playlist in playlists:
+                        if playlist['owner']['id'] != 'spotify':
+                            db.session.add(Playlist(playlist_id=playlist['id'], name=playlist['name'], owner=playlist['owner']['id']))
+                            db.session.add(User(playlist['owner']['id'], "", "", "", "", "", country=countries[i]))
+                            db.session.commit()
 
-
-def get_get_user_tracks():
-  return spotify.current_user_saved_tracks()
-
-
-class SpotifyUserSongInPlaylist:
-    # __tablename__ = 'spotifyusersonginplaylist'
-    # user_id = db.Column(db.String())
-    # date_added = db.Column(db.DateTime())
-    # track_id = db.Column(db.String())
-    # popularity = db.Column(db.Integer())
-    # explicit = db.Column(db.Boolean())
-
-    def __init__(self, user_id, date_added, track_id, popularity, explicit):
-        self.user_id = user_id
-        self.date_added = date_added
-        self.track_id = track_id
-        self.popularity = popularity
-        self.explicit = explicit
+        except Exception as e:
+            print(e)
+        print("finished with country: "+ countries[i-1])
---
+++ models.py
@@ -0,0 +1,51 @@
+from application import db
+from sqlalchemy.dialects.postgresql import JSON
+
+class SpotifyUserSongInPlaylist(db.Model):
+    __tablename__ = 'spotifyusersonginplaylist'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String())
+    date_added = db.Column(db.DateTime())
+    track_id = db.Column(db.String())
+    popularity = db.Column(db.Integer())
+    explicit = db.Column(db.Boolean())
+
+    def __init__(self, user_id, date_added, track_id, popularity, explicit):
+        self.user_id = user_id
+        self.date_added = date_added
+        self.track_id = track_id
+        self.popularity = popularity
+        self.explicit = explicit
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String())
+    mind_aspect = db.Column(db.String(), nullable=True)
+    energy_aspect = db.Column(db.String(), nullable=True)
+    nature_aspect = db.Column(db.String(), nullable=True)
+    tactics_aspect = db.Column(db.String(), nullable=True)
+    identity_aspect = db.Column(db.String(), nullable=True)
+    country = db.Column(db.String())
+
+    def __init__(self, user_id, mind_aspect, energy_aspect, nature_aspect, tactics_aspect, identity_aspect, country):
+        self.user_id = user_id
+        self.identity_aspect = identity_aspect
+        self.tactics_aspect = tactics_aspect
+        self.nature_aspect = nature_aspect
+        self.mind_aspect = mind_aspect
+        self.energy_aspect = energy_aspect
+        self.country = country
+
+class Playlist(db.Model):
+    __tablename__ = 'playlist'
+    id = db.Column(db.Integer, primary_key=True)
+    playlist_id = db.Column(db.String())
+    name = db.Column(db.String())
+    owner = db.Column(db.String())
+
+    def __init__(self, playlist_id, name, owner):
+        self.playlist_id = playlist_id
+        self.name = name
+        self.owner = owner
